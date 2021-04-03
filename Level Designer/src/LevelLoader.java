85511dc2cf697d3b9ad1cdfceca2d5a064a5a8d4
Gunnar Funderburk <gfunderburk@protonmail.com>
2021-04-03 17:29:44 -0400

    Level Loader work

--- Level Designer/src/LevelLoader.java
+++ Level Designer/src/LevelLoader.java
@@ -9,16 +9,20 @@ public class LevelLoader {
      * Purpose: Save a current level from the level designer  
      */
     public void save(){
+        throw new RuntimeException("Method Not Implemented");
 
 
     }
      /**
-     * save
+     * load
      * Input: nameofFile, name of File so that the loader knows which file to load 
      * Return: None
-     * Purpose: Save a current level from the level designer  
+     * Purpose:Loads a level into the designer for modification 
      */
     public AllObject load(){
+        throw new RuntimeException("Method Not Implemented");
+        
+        
 
     }
     
---
+++ Level Designer/src/model/Level.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Level {
+    
+    String levelName;
+    String levelPhotoPath;
+    int numFish;
+    boolean bossFish;
+
+
+    ArrayList<Obstacle> obstacles=new ArrayList<Obstacle>();
+    ArrayList<Food> orbs=new ArrayList<Food>();
