a902814c74de6bb12e6cd0c3c81887c69a1c23db
Gunnar Funderburk <gfunderburk@protonmail.com>
2021-04-11 00:27:24 -0400

    Renamed main folder
    Main Folder was named javafx so I changed it to Game

---
+++ Game/src/model/FishGame.java
@@ -0,0 +1,423 @@
+package model;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FishGame {
+
+    // the storage of all things in the sceen.
+    // first element of list will be user's fish;
+    // while using for loops don't cover first element.
+    ArrayList<AllObject> objectStorage;
+
+    // three basic value for the game
+    // from class we learn this week those variable may change to intproperty in
+    // order to bind
+
+    //userfish
+    Userfish user;
+
+    public Userfish getUser() {
+        return this.user;
+    }
+
+    public void setUser(Userfish user) {
+        this.user = user;
+    }
+
+    static int life;
+    static int points = 0; 
+    static int health;
+
+    //Level static global variable
+    public static int level;
+
+
+    // How many food on the screen for now 
+    int numberOfFood;
+
+    // iHow many each kind of fish on the screen for now
+    int numberOfType1Fish;
+    int numberOfType2Fish;
+    int numberOfType3Fish;
+    int numberOfPoisonFish;
+
+    public int getNumberOfPoisonFish() {
+        return this.numberOfPoisonFish;
+    }
+
+    public void setNumberOfPoisonFish(int numberOfPoisonFish) {
+        this.numberOfPoisonFish = numberOfPoisonFish;
+    }
+
+    // make sure there is not too many food on the screen
+    int limitOfFood;
+
+    public int getLimitOfFood() {
+        return this.limitOfFood;
+    }
+
+    public void setLimitOfFood(int limitOfFood) {
+        this.limitOfFood = limitOfFood;
+    }
+
+    // it use to make sure there is no too much each kind of fish in the screen
+    int limitOfType1Fish;
+    int limitOfType2Fish;
+    int limitOfType3Fish;
+    int limitOfPoisonFish;
+
+    public int getLimitOfType1Fish() {
+        return this.limitOfType1Fish;
+    }
+
+    public void setLimitOfType1Fish(int limitOfType1Fish) {
+        this.limitOfType1Fish = limitOfType1Fish;
+    }
+
+    public int getLimitOfType2Fish() {
+        return this.limitOfType2Fish;
+    }
+
+    public void setLimitOfType2Fish(int limitOfType2Fish) {
+        this.limitOfType2Fish = limitOfType2Fish;
+    }
+
+    public int getLimitOfType3Fish() {
+        return this.limitOfType3Fish;
+    }
+
+    public void setLimitOfType3Fish(int limitOfType3Fish) {
+        this.limitOfType3Fish = limitOfType3Fish;
+    }
+
+    public int getLimitOfPoisonFish() {
+        return this.limitOfPoisonFish;
+    }
+
+    public void setLimitOfPoisonFish(int limitOfPoisonFish) {
+        this.limitOfPoisonFish = limitOfPoisonFish;
+    }
+
+
+    // to bolean for state for game,
+    // if isCheatModeOn = true, user fish will not die
+    // if isGameGver = true either use win or lose,
+    boolean isCheatModeOn;
+    boolean isGameOver;
+
+    // basic constructor to start game if there is not input yet, purpose for
+    // testing the program.
+    public FishGame() {
+        isCheatModeOn = false;
+        isGameOver = false;
+        objectStorage = new ArrayList<AllObject>();
+        user = new Userfish(Type.FishType1, 5, 1, 30);
+    }
+
+    // constructor while game start, model will recieve different value from
+    // different level. however other limitation such as speed, will not in
+    // constructor because each kind of fish have its own speed and their initial
+    // speed in their own object;
+    public FishGame(int life, int health, int limitOfFood, int limitOfType1Fish, int limitOfType2Fish,
+            int limitOfType3Fish, int limitOfPoisonFish) {
+        FishGame.life = life;
+        FishGame.health = health;
+        isCheatModeOn = false;
+        isGameOver = false;
+        this.limitOfFood = limitOfFood;
+        this.limitOfType1Fish = limitOfType1Fish;
+        this.limitOfType2Fish = limitOfType2Fish;
+        this.limitOfType3Fish = limitOfType3Fish;
+        this.limitOfPoisonFish = limitOfPoisonFish;
+        objectStorage = new ArrayList<AllObject>();
+        for (int i = 0; i < limitOfFood; i++){
+            objectStorage.add(new Food());
+            numberOfFood += 1;
+        }
+        for (int i = 0; i < limitOfType1Fish; i++){
+            objectStorage.add(new Fishes(Type.FishType1, 7, 1, 50));
+            numberOfType1Fish += 1;
+        }
+        for (int i = 0; i < limitOfType2Fish; i++){
+            objectStorage.add(new Fishes(Type.FishType2, 6, 2, 100));
+            numberOfType2Fish += 1;
+        }
+        for (int i = 0; i < limitOfType3Fish; i++){
+            objectStorage.add(new Fishes(Type.FishType3, 10, 3, 150));
+            numberOfType3Fish += 1;
+        }
+        for (int i = 0; i < limitOfPoisonFish; i++){
+            objectStorage.add(new Fishes(Type.PoisonFish, 7, 1, 50));
+            numberOfPoisonFish += 1;
+        }
+        user = new Userfish(Type.FishType1, 5, 1, 30);
+
+    }
+
+    // from x and y we know image position, from image size we know how much it
+    // covers
+    // x and y + size x size we can get the area of fish that other fish can't
+    // touches.
+    // remind and undeterming, size of the fish will be grow, we need careful about
+    // this.
+    public ArrayList<Integer> Fishmeet() {
+        ArrayList<Integer> idToRemove = new ArrayList<Integer>();
+        ArrayList<AllObject> removeList = new ArrayList<>();
+        for (int i = 0; i < objectStorage.size(); i++ ){
+            for (int o = 0; o < objectStorage.size(); o++ ){
+                if ( i != o){
+                    if (FishGame.circleChecking(objectStorage.get(i).drawCircle(), objectStorage.get(o).drawCircle()) != -1){
+                        if (situationHandle(objectStorage.get(i), objectStorage.get(o)).length != 0){
+                            for (AllObject a : situationHandle(objectStorage.get(i), objectStorage.get(o))){
+                                idToRemove.add(a.getId());
+                                removeList.add(a);
+                                if ((a.getType() == Type.Food)) {
+                                    numberOfFood -= 1;
+                                }else if ((a.getType() == Type.FishType1)){
+                                    numberOfType1Fish -=1;
+                                }else if ((a.getType() == Type.FishType2)){
+                                    numberOfType2Fish -=1;
+                                }else if ((a.getType() == Type.FishType3)){
+                                    numberOfType3Fish -=1;
+                                }else if (a.getType() == Type.PoisonFish){
+                                    numberOfPoisonFish -= 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (AllObject needRmove :  removeList){
+            objectStorage.remove(needRmove);
+        }
+        return idToRemove;
+    }
+
+    public void userfishcollision(){
+        for (AllObject a : objectStorage){
+            if (FishGame.circleChecking(user.drawCircle(), a.drawCircle()) != -1){
+                if (user.getSize() == a.getSize()){
+                    health -= 1;
+                }else if(user.getSize() < a.getSize()){
+                    health -= (a.getSize() - user.getSize());
+                }else if(user.getSize() > a.getSize()){
+                    user.eat(a);
+                }
+            if (FishGame.health < 1){
+                FishGame.life -= 1;
+                if (FishGame.life < 1){
+                    if (isCheatModeOn = false){
+                        isGameOver = true;
+                }
+
+                }
+            }
+            }
+        }
+    }
+
+    public static int circleChecking(int[] circle1, int[] circle2){
+        int dispostion = (circle1[0] - circle2[0])*(circle1[0] - circle2[0]) + (circle1[1] -circle2[1]) *  (circle1[1] -circle2[1]);
+        int radiussum = (circle1[2] + circle2[2]) * (circle1[2] + circle2[2]);
+        if (dispostion == radiussum){
+            return 1;
+        }else if(dispostion > radiussum){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+    // handle all kinds of situation while different things met
+    // return        first object eat second object
+    // return        second object eat first object
+    // return        first object is obstacles and second object is not fish
+    // return        seoncond object is obstacles and first object is not fish
+    // else do nothing
+
+    public static AllObject[] situationHandle(AllObject firstobject, AllObject secondObject){
+        AllObject[] eaten = new AllObject[]{};
+        if (firstobject.getType() == Type.Shark){
+            if (secondObject.getType() != Type.Obstacles){
+                eaten = ((Shark) firstobject).Sharkeat(secondObject);
+            }
+        }
+        else if(secondObject.getType() == Type.Shark){
+            if (firstobject.getType() != Type.Obstacles){
+                eaten = ((Shark) secondObject).Sharkeat(firstobject);
+            }
+        }
+        else if (
+            (firstobject.getSize() > secondObject.getSize()||
+            (firstobject instanceof Fishes && secondObject.getType() == Type.Food))){
+                eaten = ((Fishes) firstobject).eat(secondObject);
+        }else if(
+            (secondObject.getSize() > firstobject.getSize()||
+            (secondObject instanceof Fishes && firstobject.getType() == Type.Food))){
+                eaten = ((Fishes) secondObject).eat(firstobject);
+        }else if(
+            (firstobject.getType() == Type.Obstacles) && (secondObject instanceof Fishes == false) && (secondObject.getType() != Type.Shark)
+        ){
+            secondObject.setSpeed(0);
+        }else if(
+            (secondObject.getType() == Type.Obstacles) && (firstobject instanceof Fishes)
+        ){
+            firstobject.ChangeSpeedAndDirection();
+        }
+        return eaten;
+    }
+
+    public void increaseSize(){
+        if (FishGame.points == 10){
+            user.setSize(user.getSize()+1);
+            user.setImageSize(user.getImageSize()+30);
+        }else if(FishGame.points == 40){
+            user.setSize(user.getSize()+1);
+            user.setImageSize(user.getImageSize()+30);
+        }else if(FishGame.points == 80){
+            user.setSize(user.getSize()+1);
+            user.setImageSize(user.getImageSize()+30);
+        }else if(FishGame.points == 110){
+            user.setSize(user.getSize()+1);
+            user.setImageSize(user.getImageSize()+30);
+        }
+    }
+
+
+    // block the area that fish can't go cross, will not use in early level,
+    // lt can work on later of the project.
+    public void blockingArea() {
+    }
+
+    public void add(AllObject a){
+        objectStorage.add(a);
+    }
+
+    
+    public void remove(AllObject a){
+        objectStorage.remove(a);
+    }
+
+    //turns cheat mode on
+    public void enableCheatMode(){
+        if(isCheatModeOn == false) {
+            isCheatModeOn = true;
+        }
+    }
+    //turns cheat mode off
+    public void disableCheatMode(){
+        if(isCheatModeOn == true) {
+            isCheatModeOn = false;
+        }
+    }
+
+    //add shark into fishGame list 
+    public void addShark(){
+        objectStorage.add(new Shark());
+    }
+
+    public void updata(){
+        for (AllObject a: objectStorage){
+            a.updatePosition();
+        }
+    }
+
+    //load and save, I don't know yet
+    public void load(){
+        // A loop through the save file which will spawn all the objects contained within
+
+    }
+
+    public void save(){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("save.game", true));
+            } catch (Exception e) {
+            //TODO: this breaks model/view separation
+            System.out.println("Save just died");
+        }
+    }
+
+    // all getters and setters
+    public int getNumberOfFood() {
+        return this.numberOfFood;
+    }
+
+    public void setNumberOfFood(int numberOfFood) {
+        this.numberOfFood = numberOfFood;
+    }
+
+    public int getNumberOfType1Fish() {
+        return this.numberOfType1Fish;
+    }
+
+    public void setNumberOfType1Fish(int numberOfType1Fish) {
+        this.numberOfType1Fish = numberOfType1Fish;
+    }
+
+    public int getNumberOfType2Fish() {
+        return this.numberOfType2Fish;
+    }
+
+    public void setNumberOfType2Fish(int numberOfType2Fish) {
+        this.numberOfType2Fish = numberOfType2Fish;
+    }
+
+    public int getNumberOfType3Fish() {
+        return this.numberOfType3Fish;
+    }
+
+    public void setNumberOfType3Fish(int numberOfType3Fish) {
+        this.numberOfType3Fish = numberOfType3Fish;
+    }
+
+    public ArrayList<AllObject> getObjectStorage() {
+        return this.objectStorage;
+    }
+
+    public void setObjectStorage(ArrayList<AllObject> objectStorage) {
+        this.objectStorage = objectStorage;
+    }
+
+    public int getLife() {
+        return this.life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean getIsCheatModeOn() {
+        return this.isCheatModeOn;
+    }
+
+    public void setIsCheatModeOn(boolean isCheatModeOn) {
+        this.isCheatModeOn = isCheatModeOn;
+    }
+
+    public boolean getIsGameOver() {
+        return this.isGameOver;
+    }
+
+    public void setIsGameOver(boolean isGameOver) {
+        this.isGameOver = isGameOver;
+    }
+}
---
+++ Game/src/model/Fishes.java
@@ -0,0 +1,176 @@
+package model;
+
+import java.util.Random;
+
+import javax.naming.directory.DirContext;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+
+public class Fishes extends AllObject {
+
+    //initialdirction of fish final?
+    private int initialDirection;
+
+    public int getInitialDirection() {
+        return this.initialDirection;
+    }
+
+    public void setInitialDirection(int initialDirection) {
+        this.initialDirection = initialDirection;
+    }
+
+    // each fish is on their owe movement.
+    // they will change speed and direction every 3 seconds. It should be called
+    // every 3s in timeline at viewclass
+    // And for speed, there is the limitation,
+    // it can't increasing too much or stop
+    // for direction, we import fish for left edge and right edge of screen.
+    // we purpose want to make fish go cross the screen,
+    // Therefore the random for direction will be 66% go for initial direction,
+    // 33% go for opposite direction.
+    // Also Our fish will not go up and down, we are try to much them slightly go up
+    // and down while they are move cross the creen
+
+    // remember limitation of dirction number;
+
+    public Fishes(Type objType, int speed, int size, int imageSize){
+        this.speed = speed;
+        this.size = size;
+        this.objtype = objType;
+        this.imageSize = imageSize;
+
+        // random the inital direction.
+        //inital x, intial y, intial initalDirection, intial direction
+        var rand = new Random();
+        int ran = rand.nextInt(2);
+        if (ran == 0) {
+            // remember import from left edge of screen
+            initialDirection = 0 + rand.nextInt(10);
+            x = new SimpleIntegerProperty(1-imageSize);
+            int yuse = rand.nextInt(1000);
+            y = new SimpleIntegerProperty(yuse);
+            direction = initialDirection;
+        } else {
+            // remember import from right edge of screen
+            initialDirection = 180 + rand.nextInt(10);
+            x = new SimpleIntegerProperty(1);
+            int yuse = rand.nextInt(1000);
+            y = new SimpleIntegerProperty(yuse);
+            direction = initialDirection;
+        }
+    }
+
+    @Override
+    public void ChangeSpeedAndDirection() {
+        randDirection();
+        randSpeed();
+    }
+
+    public void randDirection() {
+        var rand = new Random();
+        var ran = rand.nextInt(3);
+        var ran2 = rand.nextInt(10);
+        var ran3 = rand.nextInt(2);
+        switch (ran) {
+        case 0:
+            if (direction <= 180) {
+                direction += 180;
+                if(360 - direction < 10){
+                    direction -= ran2;
+                }else if (360 - direction < 30){
+                    if(ran3 == 0){
+                        direction -= ran2;
+                    }else{
+                        direction += ran2;
+                    }
+                }else{
+                    direction += ran2;
+                }
+            } else {
+                direction -= 180;
+                if (direction-180 < 10){
+                    direction += ran2;
+                }else if (direction-180 < 30){
+                    if(ran3 == 0){
+                        direction -= ran2;
+                    }else{
+                        direction += ran2;
+                    }
+                }else{
+                    direction -= ran2;
+                }
+            }
+            break;
+        case 1:
+        case 2:
+        if (direction <= 180) {
+            if(360 - direction < 10){
+                direction -= ran2;
+            }else if (360 - direction < 30){
+                if(ran3 == 0){
+                    direction -= ran2;
+                }else{
+                    direction += ran2;
+                }
+            }else{
+                direction += ran2;
+            }
+        } else {
+            if (direction-180 < 10){
+                direction += ran2;
+            }else if (direction-180 < 30){
+                if(ran3 == 0){
+                    direction -= ran2;
+                }else{
+                    direction += ran2;
+                }
+            }else{
+                direction -= ran2;
+            }
+        }
+            break;
+        }
+    }
+
+    public AllObject[] eat(AllObject a){
+        AllObject[] stor = new AllObject[]{};
+        if (a.getType() == Type.PoisonFish||a.getType() == Type.Mine){
+            stor = new AllObject[2];
+            stor[0] = this;
+            stor[1] = a;
+        }else if((a instanceof Fishes) && a.getType() != Type.PoisonFish){
+            stor = new AllObject[1];
+            stor[0] = a;
+        }
+        return stor;
+    }
+
+    // change randon speed for fish, 
+    // remember up bound and low bound limiation onfish.
+    public void randSpeed() {
+        var rand = new Random();
+        int ran = rand.nextInt(2);
+        int ran2 = rand.nextInt(10);
+        if(ran == 0){
+            this.speed -= ran2;
+        }else{
+            this.speed += ran2;
+        }
+    }
+    // returns a string for the save function
+    public String serialize() {
+        String dataString = "";
+        dataString += this.objtype.toString();
+        dataString += ":";
+        dataString += Integer.toString(x.get()) + "," + Integer.toString(y.get());
+        dataString += ":";
+        dataString += Integer.toString(this.size);
+        dataString += ":";
+        dataString += Integer.toString(direction);
+        dataString += ":";
+        dataString += Integer.toString(this.imageSize);
+         
+        return dataString;
+    }
+}
