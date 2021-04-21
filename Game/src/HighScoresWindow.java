30b7a9c78ae09dd54184d3fab21d40e5c5ad68d6
Blake Alexander <blakefa12@icloud.com>
2021-04-21 14:44:20 -0400

    high scores

--- Game/src/HighScoresWindow.java
+++ Game/src/HighScoresWindow.java
@@ -4,12 +4,18 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.text.FontWeight;
+
 
 public class HighScoresWindow {
 
@@ -28,12 +34,23 @@ public class HighScoresWindow {
     @FXML
     VBox score;
 
+    @FXML
+    Label hiScore;
+
     public void initialize() {
         // BackgroundFill backgroundFill = new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY);
         // Background background = new Background(backgroundFill);
         // backgroundBox.setBackground(background);
 
-        imageHS.setPreserveRatio(true);
+        InnerShadow innerShadow = new InnerShadow();
+        innerShadow.setOffsetX(4);
+        innerShadow.setOffsetY(4);
+        innerShadow.setColor(Color.web("0x3b596d"));
+       
+        hiScore.setEffect(innerShadow);
+        hiScore.setText("High Scores");
+        hiScore.setTextFill(Color.color(0.5, .8, 1));
+        hiScore.setFont(Font.font("Verdana", FontWeight.BOLD, 55));
 
         HighScoreManager scores = new HighScoreManager();
         int count = 0;
